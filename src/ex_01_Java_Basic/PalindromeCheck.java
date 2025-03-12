6f4e22edf27344c5956437aa2d1e197f2aee265d
basshaw <basantshaw96@gmail.com>
2025-03-12 22:48:48 +0530

    12 Mar 2025

---
+++ src/ex_01_Java_Basic/PalindromeCheck.java
@@ -0,0 +1,48 @@
+package ex_01_Java_Basic;
+import java.util.*;
+
+public class PalindromeCheck {
+    public static void main (String [] args){
+        System.out.println("Palindrome Checker:\nEnter a String-");
+        Scanner sc=new Scanner(System.in);
+
+        //Take a String input
+        String stringInput=sc.nextLine();
+
+        //Declare a reverse array variable
+        char[] reverseArray=new char[stringInput.length()];
+
+        //Efficient Way to reverse a string is through using StringBuilder
+        //String reversedString = new StringBuilder(stringInput).reverse().toString();
+
+        /*
+        StringBuilder is a mutable sequence of characters.
+        Unlike String, which is immutable,
+        StringBuilder allows modifications (like reversal) without creating multiple objects in memory.
+        Wrapping stringInput inside StringBuilder lets us use its built-in methods.
+        */
+
+        for(int i=stringInput.length()-1;i>=0;i--){
+            reverseArray[stringInput.length()-1-i]=stringInput.charAt(i);
+
+        }
+
+        //The String class has a constructor that takes a char[] and creates a String from it.
+
+        String reversedString= new String(reverseArray);
+        if (stringInput.equalsIgnoreCase(reversedString)) {
+            System.out.println("Input string is Palindrome");}
+        else{
+            System.out.println("Input string is not Palindrome");
+            System.out.println("Input string:"+stringInput);
+            System.out.println("Reverse string:"+reversedString);
+
+        }
+    }
+}
+
+
+
+/*Take a String input
+Declare a reverse array variable
+run a for loop which would pick char from input string and add it to reverse array */
